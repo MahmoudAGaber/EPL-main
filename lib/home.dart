0f3d0b05aa03e453b1da0aed51c4c554f804daf2
MahmoudAGaber <mahmoud.a.gaber569@gmail.com>
2022-11-21 14:52:16 +0200

    h5

--- lib/home.dart
+++ lib/home.dart
@@ -7,6 +7,7 @@ import 'package:arseli/news.dart';
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:get/get.dart';
+import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 import 'package:provider/provider.dart';
 import 'package:intl/intl.dart';
 import 'package:intl/date_symbol_data_local.dart';
@@ -25,23 +26,27 @@ class home extends StatefulWidget {
   _homeState createState() => _homeState();
 }
 
-class _homeState extends State<home> {
+class _homeState extends State<home> with SingleTickerProviderStateMixin {
+
   LeaguesViewModel leaguesViewModel;
   VideosViewModel videosViewModel;
   NewsViewModel newsViewModel;
   MatchesViewModel matchesViewModel;
-  PageController _pageController;
+
   List<Widget> _childern = [];
 
-  int _selectedIndex = 0;
-  void _onPageChanged(int index) {
-    setState(() {
-      _selectedIndex= index;
-    });
-  }
 
-  void _onItemTapped(int selectedIndex) {
-    _pageController.jumpToPage(selectedIndex);
+
+  int _selectedIndex = 0;
+
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+    static List<Widget> _widgetOptions;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 
 
@@ -53,22 +58,22 @@ class _homeState extends State<home> {
 
   @override
   void initState() {
-    _childern = [
-      Matches(
-        dateTime: widget.dateTime,
-      ),
-      news(),
-      // Reels(),
-      Videos(),
-      Favourites(tag: 'الفرق'),
-      Leagues(tag: 'البطولات',),
 
-      // More(),
+    _widgetOptions = <Widget>[
+    Matches(
+    dateTime: widget.dateTime,
+    ),
+    news(),
+    // Reels(),
+    Videos(),
+    Favourites(tag: 'الفرق'),
+    Leagues(tag: 'البطولات',),
     ];
 
-    _pageController = PageController(initialPage: widget.index,keepPage: true);
+
 
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+
       setState(() {
         initializeDateFormatting("EN_SA", null).then((_) {
           var now = new DateTime.now();
@@ -89,181 +94,182 @@ class _homeState extends State<home> {
       //videosViewModel.getVideos('تقارير ومؤتمرات صحفية', 0, "");
       leaguesViewModel.getDataLeagues();
     });
+
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    //print('heeerrree');
     return Directionality(
       textDirection:
           locale == 'US' ? ui.TextDirection.ltr : ui.TextDirection.rtl,
       child: Scaffold(
-        body: PageView(
-          controller: _pageController,
-          children: _childern,
-          physics: NeverScrollableScrollPhysics(),
-          onPageChanged: _onPageChanged,
-        ),
-        bottomNavigationBar: SizedBox(
-          child: BottomNavigationBar(
-            showSelectedLabels: false,
-            showUnselectedLabels: false,
-            elevation: 10.0,
-            onTap: _onItemTapped,
-            items: [
-              BottomNavigationBarItem(
-                icon: Padding(
-                  padding: const EdgeInsets.only(right: 0,),
-                  child: new Container(
-                    height: 22,width: 22,
-                    child: Image.asset('assets/Icons/Mask Group 232.png',
-                      color: _selectedIndex == 0
+        body: Center(child: _widgetOptions.elementAt(_selectedIndex),),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.shifting,
+          items: [
+            BottomNavigationBarItem(
+              icon: Padding(
+                padding: const EdgeInsets.only(right: 0,),
+                child: _selectedIndex==0?Container(
+                  width: 25,height: 25,
+                  child: Image.asset('assets/Icons/Mask Group 232.png',
+                    color: _selectedIndex == 0
+                      ? Color.fromRGBO(95, 31, 117, 1.0)
+                      : Colors.grey,),
+                ):Container(
+                  width: 22,height: 22,
+                  child: Image.asset('assets/Icons/Mask Group 232.png',
+                    color: _selectedIndex == 0
                         ? Color.fromRGBO(95, 31, 117, 1.0)
                         : Colors.grey,),
+                ),
+              ),
+              label: "المباريات".tr,
+
+            ),
+            BottomNavigationBarItem(
+                icon: Padding(
+                  padding: const EdgeInsets.only(right: 0,left: 0),
+                  child: new Container(
+                    height: 19,width: 19,
+                    child: Image.asset('assets/Icons/Mask Group 236.png',
+                      color: _selectedIndex == 1
+                          ? Color.fromRGBO(95, 31, 117, 1.0)
+                          : Colors.grey,),
                   ),
                 ),
-                label: "المباريات".tr,
-
-              ),
-              BottomNavigationBarItem(
-                  icon: Padding(
-                    padding: const EdgeInsets.only(right: 0,left: 12),
-                    child: new Container(
-                      height: 19,width: 19,
-                      child: Image.asset('assets/Icons/Mask Group 236.png',
-                        color: _selectedIndex == 1
-                            ? Color.fromRGBO(95, 31, 117, 1.0)
-                            : Colors.grey,),
-                    ),
-                  ),
-                  label: "الأخبار".tr
-                  // title: Padding(
-                  //   padding: const EdgeInsets.only(left: 10),
-                  //   child: new Text(
-                  //     "الأخبار".tr,
-                  //     style: TextStyle(                      fontFamily: 'Vazirmatn',
-                  //         fontWeight: FontWeight.w400,
-                  //         color: _selectedIndex == 1
-                  //             ? Color.fromRGBO(95, 31, 117, 1.0)
-                  //             : Colors.grey),
-                  //   ),
-                  // )
-                  ),
-              /*
-              BottomNavigationBarItem(
-                  icon:Padding(
-                    padding: const EdgeInsets.only(left: 10),
-                    child: Container(
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color:_selectedIndex == 2
-                              ? Color.fromRGBO(95, 31, 117, 1.0)
-                              :Colors.white
-                      ),
-
-                      child: Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: Icon(Icons.video_collection_outlined,
-                          color: _selectedIndex == 2
-                              ? Colors.white
-                              : Colors.grey
-                          ,size: 35, ),
-                      ),
-                    ),
-                  ),
-                  label: "Reels".tr
-                  // title: Padding(
-                  //   padding: const EdgeInsets.only(left: 10),
-                  //   child: new Text(
-                  //     "Reels".tr,
-                  //     style: TextStyle(                      fontFamily: 'Vazirmatn',
-                  //         fontWeight: FontWeight.w400,
-                  //         color: _selectedIndex == 2
-                  //             ? Color.fromRGBO(95, 31, 117, 1.0)
-                  //             : Colors.grey),
-                  //   ),
-                  // )
-              ),
-
-               */
-              BottomNavigationBarItem(
-                  icon: Padding(
-                    padding: const EdgeInsets.only(right: 0,),
-                    child: new Container(
-                      height: 23,width: 23,
-                      child: Image.asset('assets/Icons/Mask Group 235.png',
-                        color: _selectedIndex == 2
-                            ? Color.fromRGBO(95, 31, 117, 1.0)
-                            : Colors.grey,),
-                    ),
-                  ),
-                  label: "الفيديو".tr
-                  // title: Padding(
-                  //   padding: const EdgeInsets.only(left: 10),
-                  //   child: new Text(
-                  //     "الفيديو".tr,
-                  //     style: TextStyle(                      fontFamily: 'Vazirmatn',
-                  //         fontWeight: FontWeight.w400,
-                  //         color: _selectedIndex == 3
-                  //             ? Color.fromRGBO(95, 31, 117, 1.0)
-                  //             : Colors.grey),
-                  //   ),
-                  // )
-                  ),
-              BottomNavigationBarItem(
-                  icon: Padding(
-                    padding: const EdgeInsets.only(right: 0,),
-                    child: new Container(
-                      height: 20,width: 20,
-                      child: Image.asset('assets/Icons/Mask Group 239.png',
-                        color: _selectedIndex == 3
-                            ? Color.fromRGBO(95, 31, 117, 1.0)
-                            : Colors.grey,),
-                    ),
-                  ),
-                  label: "المفضلة".tr
-                  // title: new Text(
-                  //   "البطولات".tr,
-                  //   style: TextStyle(                      fontFamily: 'Vazirmatn',
-                  //       fontWeight: FontWeight.w400,
-                  //       color: _selectedIndex == 4
-                  //           ? Color.fromRGBO(95, 31, 117, 1.0)
-                  //           : Colors.grey),
-                  // )
-                  ),
-              BottomNavigationBarItem(
-                  icon: Padding(
-                    padding: const EdgeInsets.only(right: 0,),
-                    child: new Container(
-                      height: 20,width: 20,
-                      child: Image.asset('assets/Icons/Mask Group 237.png',
-                        color: _selectedIndex == 4
-                            ? Color.fromRGBO(95, 31, 117, 1.0)
-                            : Colors.grey,),
-                    ),
-                  ),
-                  label: "البطولات".tr),
-
-              /*
+                label: "الأخبار".tr
+                // title: Padding(
+                //   padding: const EdgeInsets.only(left: 10),
+                //   child: new Text(
+                //     "الأخبار".tr,
+                //     style: TextStyle(                      fontFamily: 'Vazirmatn',
+                //         fontWeight: FontWeight.w400,
+                //         color: _selectedIndex == 1
+                //             ? Color.fromRGBO(95, 31, 117, 1.0)
+                //             : Colors.grey),
+                //   ),
+                // )
+                ),
+            /*
             BottomNavigationBarItem(
-              icon: Icon(Icons.menu,
-                  color: _selectedIndex == 4
-                      ? Color.fromRGBO(95, 31, 117, 1.0)
-                      : Colors.grey),
-              title: Text(
-                "المزيد".tr,
-                style: TextStyle(                      fontFamily: 'Vazirmatn',
-                    fontWeight: FontWeight.w400,
-                    color: _selectedIndex == 4
-                        ? Color.fromRGBO(95, 31, 117, 1.0)
-                        : Colors.grey),
-              ),
+                icon:Padding(
+                  padding: const EdgeInsets.only(left: 10),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color:_selectedIndex == 2
+                            ? Color.fromRGBO(95, 31, 117, 1.0)
+                            :Colors.white
+                    ),
+
+                    child: Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Icon(Icons.video_collection_outlined,
+                        color: _selectedIndex == 2
+                            ? Colors.white
+                            : Colors.grey
+                        ,size: 35, ),
+                    ),
+                  ),
+                ),
+                label: "Reels".tr
+                // title: Padding(
+                //   padding: const EdgeInsets.only(left: 10),
+                //   child: new Text(
+                //     "Reels".tr,
+                //     style: TextStyle(                      fontFamily: 'Vazirmatn',
+                //         fontWeight: FontWeight.w400,
+                //         color: _selectedIndex == 2
+                //             ? Color.fromRGBO(95, 31, 117, 1.0)
+                //             : Colors.grey),
+                //   ),
+                // )
             ),
 
              */
-            ],
+            BottomNavigationBarItem(
+                icon: Padding(
+                  padding: const EdgeInsets.only(right: 0,),
+                  child: new Container(
+                    height: 23,width: 23,
+                    child: Image.asset('assets/Icons/Mask Group 235.png',
+                      color: _selectedIndex == 2
+                          ? Color.fromRGBO(95, 31, 117, 1.0)
+                          : Colors.grey,),
+                  ),
+                ),
+                label: "الفيديو".tr
+                // title: Padding(
+                //   padding: const EdgeInsets.only(left: 10),
+                //   child: new Text(
+                //     "الفيديو".tr,
+                //     style: TextStyle(                      fontFamily: 'Vazirmatn',
+                //         fontWeight: FontWeight.w400,
+                //         color: _selectedIndex == 3
+                //             ? Color.fromRGBO(95, 31, 117, 1.0)
+                //             : Colors.grey),
+                //   ),
+                // )
+                ),
+            BottomNavigationBarItem(
+                icon: Padding(
+                  padding: const EdgeInsets.only(right: 0,),
+                  child: new Container(
+                    height: 20,width: 20,
+                    child: Image.asset('assets/Icons/Mask Group 239.png',
+                      color: _selectedIndex == 3
+                          ? Color.fromRGBO(95, 31, 117, 1.0)
+                          : Colors.grey,),
+                  ),
+                ),
+                label: "المفضلة".tr
+                // title: new Text(
+                //   "البطولات".tr,
+                //   style: TextStyle(                      fontFamily: 'Vazirmatn',
+                //       fontWeight: FontWeight.w400,
+                //       color: _selectedIndex == 4
+                //           ? Color.fromRGBO(95, 31, 117, 1.0)
+                //           : Colors.grey),
+                // )
+                ),
+            BottomNavigationBarItem(
+                icon: Padding(
+                  padding: const EdgeInsets.only(right: 0,),
+                  child: new Container(
+                    height: 20,width: 20,
+                    child: Image.asset('assets/Icons/Mask Group 237.png',
+                      color: _selectedIndex == 4
+                          ? Color.fromRGBO(95, 31, 117, 1.0)
+                          : Colors.grey,),
+                  ),
+                ),
+                label: "البطولات".tr),
+
+            /*
+          BottomNavigationBarItem(
+            icon: Icon(Icons.menu,
+                color: _selectedIndex == 4
+                    ? Color.fromRGBO(95, 31, 117, 1.0)
+                    : Colors.grey),
+            title: Text(
+              "المزيد".tr,
+              style: TextStyle(                      fontFamily: 'Vazirmatn',
+                  fontWeight: FontWeight.w400,
+                  color: _selectedIndex == 4
+                      ? Color.fromRGBO(95, 31, 117, 1.0)
+                      : Colors.grey),
+            ),
           ),
+
+           */
+          ],
+          currentIndex:  _selectedIndex,
+          selectedItemColor: Color.fromRGBO(95, 31, 117, 1.0),
+          selectedIconTheme: IconThemeData(size: 35),
+          unselectedIconTheme: IconThemeData(size: 20),
+          onTap: _onItemTapped,
         ),
       ),
     );
